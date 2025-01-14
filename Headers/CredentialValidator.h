9e2fb9e1de660dfd0e032333b1f87b0ff7d5b332
Salim Maalouf <salim.maalouf@mymonty.com>
2025-01-14 16:38:19 +0200

    no message

---
+++ Headers/CredentialValidator.h
@@ -0,0 +1,18 @@
+//
+//  CredentialValidator.h
+//  CredentialValidator
+//
+//  Created by Salim Maalouf on 14/01/2025.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CredentialValidator.
+FOUNDATION_EXPORT double CredentialValidatorVersionNumber;
+
+//! Project version string for CredentialValidator.
+FOUNDATION_EXPORT const unsigned char CredentialValidatorVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CredentialValidator/PublicHeader.h>
+
+
